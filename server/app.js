6235101ed26fa8066caf61911b534e02ee9074a1
MacTavish Lee <1064416560@qq.com>
2019-09-12 09:54:14 +0800

    :sparkle: feat: 增加对文章文档树的解析

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const Koa = require('koa')
 const path = require('path')
+const fs = require('fs')
 const koaStatic = require('koa-static')
+
+const sourceHandler = require('./utils/source-handler')
+const articleRoot = path.join(__dirname, '../articles')
+
+
+const result = sourceHandler.traverse(articleRoot, filePath => {
+  // 对markdown的解析
+})
+
+console.log(result)
 
 
 const app = new Koa()
